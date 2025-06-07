d61d18fa36563b5c88387bb6aa24a4d62f69ec67
kgwozdziowski2 <k.gwozdziowski@entirem.com>
2025-06-07 20:05:24 +0200

    auth

--- dist/src/auth/auth.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-export declare class AuthService {
-    private readonly userRepository;
-    constructor(userRepository: Repository<User>);
-    create(data: any): Promise<User>;
-    findOne(condiction: any): Promise<User | null>;
-}
